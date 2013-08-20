fbdf27846f474a395b4ed08cbc37a57bb156ce1b
rasik20051 <rasik.wankhede@outlook.com>
2013-08-21 00:36:21 +0530

    follow other users

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,2 +1,30 @@
 class RelationshipsController < ApplicationController
+
+
+	def create
+        @relationship = Relationship.new
+        @relationship.followed_id = params[:followed_id]
+        @relationship.follower_id = current_user.id</p>
+ 
+    if @relationship.save
+        redirect_to User.find params[:followed_id]
+    else
+        flash[:error] = "Couldn't Follow"
+        redirect_to root_url
+    end
+end
+
+def create
+        @relationship = Relationship.new
+        @relationship.followed_id = params[:followed_id]
+        @relationship.follower_id = current_user.id</p>
+ 
+    if @relationship.save
+        redirect_to User.find params[:followed_id]
+    else
+        flash[:error] = "Couldn't Follow"
+        redirect_to root_url
+    end
+end
+
 end
