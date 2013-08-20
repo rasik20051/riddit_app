7c53de41bb699e730f07a0e224da6ef61c4018b8
rasik20051 <rasik.wankhede@outlook.com>
2013-08-20 22:20:44 +0530

    Initial rail app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ribbitapp::Application.config.secret_key_base = 'a9331c1260996f7600194c22b678934b24cfd7382a9e2df041af0803b55228fbfddc991d5ca3ae3be33501451a58698cbee248ef35dac31a16d5a47b75f876c1'
